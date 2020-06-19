2d3c0c196a675b7a10e780c321ed9eefac1c7eee
Ph4t3 <nikhilr.v2.0@gmail.com>
2020-06-19 21:50:49 +0530

    fix for leap(im foolish)

--- cpp/leap/leap.cpp
+++ cpp/leap/leap.cpp
@@ -2,9 +2,6 @@
 
 namespace leap {
     bool is_leap_year(int year) {
-        if(!year%400 || (!year%4 && year%100)) {
-            return true;
-        }
-        return false;
+        return !year%400 || (!year%4 && year%100)
     }
 }  // namespace leap
